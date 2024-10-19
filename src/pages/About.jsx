fef93b40a16c44241801df3759a44fcf2b209acc
tasnia-shahrin <tasniashahrin269@gmail.com>
2024-10-19 23:27:15 +0600

    another commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div></div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quis rem animi! Possimus quas quibusdam illum! Laborum qui vel, placeat, itaque quos consequuntur vero laudantium pariatur architecto ipsam cupiditate quod!</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta, veniam quisquam. Autem facere possimus quisquam nulla aspernatur obcaecati modi consequatur qui optio porro! Qui illum et facere eveniet ab! Ex consequatur fugiat incidunt assumenda in laboriosam, voluptatem explicabo veritatis saepe.</p>
+          <b className='text-gray-800'>Our Vision</b>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat cumque placeat nisi cum eveniet necessitatibus obcaecati consequuntur sed provident. Et.</p>
+          
+        </div>
+      </div>
+      <div className='text-xl my-4'>
+        <p>Why <span className='text-gray-700 font-semibold'>Choose Us</span></p>
+      </div>
+      <div className='flex flex-col md:flex-row mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, placeat!</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience:</b>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, repellat?</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, maxime.</p>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 const Appointment = () => {
 
@@ -11,6 +12,7 @@ const Appointment = () => {
   const [docSlots,setDocSlots]=useState([])
   const [slotIndex,setSlotIndex]=useState(0)
   const [slotTime,setSlotTime]=useState('')
+  const daysOfWeek = ['SUN','MON','TUE','WED','THU','FRI','SAT']
 
   const fetchDocInfo= async ()=>{
     const docInfo=doctors.find(doc=> doc._id===docId);
@@ -29,7 +31,7 @@ const Appointment = () => {
 
       // setting end time of the date with index
       let endTime=new Date()
-      endTime.setDate(today.getDate()+1)
+      endTime.setDate(today.getDate()+i)
       endTime.setHours(21,0,0,0)
 
       // setting hours
@@ -37,9 +39,21 @@ const Appointment = () => {
         currentDate.setHours(currentDate.getHours()>10 ? currentDate.getHours()+1 : 10)
         currentDate.setMinutes(currentDate.getMinutes()>30?30:0)
       }else{
-        currentDate.setHours(10);
+        currentDate.setHours(10)
         currentDate.setMinutes(0)
       }
+      let timeSlots=[]
+      while(currentDate<endTime){
+        let formattedTime=currentDate.toLocaleTimeString([],{hour: '2-digit',minute:'2-digit'})
+        //add slot to array
+        timeSlots.push({
+          datetime:new Date(currentDate),
+          time:formattedTime
+        })
+        //increment current time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes()+30)
+      }
+      setDocSlots(prev=> ([...prev,timeSlots]))
     }
   }
 
@@ -50,6 +64,10 @@ const Appointment = () => {
   useEffect(()=>{
     getAvailableSlots()
   },[docInfo])
+
+  useEffect(()=>{
+    console.log(docSlots)
+  },[docSlots])
 
   return docInfo &&(
     <div>
@@ -74,6 +92,31 @@ const Appointment = () => {
           <p className='text-gray-500 font-medium mt-4'>Appointment fee: <span className='text-gray-600'>{currency}{docInfo.fees}</span></p>
         </div>
       </div>
+      {/* BOOKING SLOTS */}
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Booking slots</p>
+        <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
+          {
+            docSlots.length && docSlots.map((item,index)=>(
+              <div onClick={()=>setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex===index?'bg-primary text-white':'border border-gray-200'}`} key={index}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+          {docSlots.length>0 && docSlots[slotIndex].map((item,index)=>(
+            <p onClick={()=>setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time===slotTime?'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+              {item.time.toLowerCase()}
+            </p>
+          ))}
+        </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an Appointment</button>
+      </div>
+      {/* list of related doctors */}
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
